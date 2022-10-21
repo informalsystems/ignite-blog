1abad0c365e8b1204bd60b28773db1a57f2baf3c
Ranadeep Biswas <mail@rnbguy.at>
2022-10-21 14:23:55 +0200

    Define messages logic

--- x/blog/keeper/msg_server_create_post.go
+++ x/blog/keeper/msg_server_create_post.go
@@ -4,14 +4,23 @@ import (
 	"context"
 
 	"blog/x/blog/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Create variable of type Post
+	var post = types.Post{
+		Creator: msg.Creator,
+		Title:   msg.Title,
+		Body:    msg.Body,
+	}
 
-	return &types.MsgCreatePostResponse{}, nil
+	// Add a post to the store and get back the ID
+	id := k.AppendPost(ctx, post)
+
+	// Return the ID of the post
+	return &types.MsgCreatePostResponse{Id: id}, nil
 }
